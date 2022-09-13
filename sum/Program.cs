3cd431ca1309670b117238598eb10e01f23ca135
Maria <m.tur67@gmail.com>
2022-09-13 23:24:10 +0300

    Sum

---
+++ sum/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите 2 числа");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int sum = a + b;
+Console.WriteLine("Сумма 2х чисел равна = " + sum);
