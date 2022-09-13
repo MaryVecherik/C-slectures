a88ac0d65552b4611c0da2054b29be5f70431ce1
Maria <m.tur67@gmail.com>
2022-09-13 23:44:20 +0300

    Добавили Rundom числа

--- sum/Program.cs
+++ sum/Program.cs
@@ -1,5 +1,6 @@
-﻿Console.WriteLine("Введите 2 числа");
-int a = int.Parse(Console.ReadLine());
-int b = int.Parse(Console.ReadLine());
+﻿int a = new Random().Next(1,10); // 1, 2, 3, 4 ... 9
+Console.WriteLine(a);
+int b = new Random().Next(1,10);
+Console.WriteLine(b);
 int sum = a + b;
 Console.WriteLine("Сумма 2х чисел равна = " + sum);
