c531f76c1de037f65f1c7bce6f3ff79464eb24e1
Maria <m.tur67@gmail.com>
2022-09-14 00:03:27 +0300

    Hello user use if or else

---
+++ hello_user_name/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите ваше имя");
+string user = Console.ReadLine();
+if (user == "Маша")
+{
+   Console.WriteLine("Я люблю тебя," + " " + user + "!");
+}
+else
+Console.WriteLine("Привет" + " " + user + "!");
